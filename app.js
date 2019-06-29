b9fe2cacbc445e988c98e5bdaae5c8c7bfd4863c
Patrick Boynton <dakilangmanok@gmail.com>
2019-06-28 21:54:49 -0400

    Created an xhr object, and called open on it.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 document.getElementById('btn').addEventListener('click', loadData);
 
 function loadData() {
-    
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'data.txt', true);
 }
