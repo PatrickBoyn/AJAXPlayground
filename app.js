dd28d3b045ce7058de5b0663461c9d4d6a092096
Patrick Boynton <dakilangmanok@gmail.com>
2019-06-28 22:59:26 -0400

    Added console logs for the ready state.

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ function loadData() {
     const xhr = new XMLHttpRequest();
 
     xhr.open('GET', 'data.txt', true);
+    
+    console.log(`Ready state: ${xhr.readyState}`);
 
     xhr.onload = function () {
+        console.log(`Ready state: ${xhr.readyState}`);
         if (this.status === 200) {
             console.log(this.responseText);
         }
