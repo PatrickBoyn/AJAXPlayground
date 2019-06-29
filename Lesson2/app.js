c0ccf5c2495cf55c0aa3f4eca29f99dbf0e3327d
Patrick Boynton <dakilangmanok@gmail.com>
2019-06-29 16:16:04 -0400

    made a new xhr  object, and used the open method.

--- Lesson2/app.js
+++ Lesson2/app.js
@@ -1,5 +1,7 @@
 document.getElementById('btn-1').addEventListener('click', loadCustomer);
 
 function loadCustomer(e) {
-    
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'customer.json', true);
 }
