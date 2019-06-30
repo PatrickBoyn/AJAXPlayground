3bfc31f784b0df9b69e59106c7ff67b914f338b7
Patrick Boynton <dakilangmanok@gmail.com>
2019-06-30 00:34:14 -0400

    Removed the synchronous code.

--- Lesson4/app.js
+++ Lesson4/app.js
@@ -3,23 +3,3 @@ const posts = [
     {title: 'Post two', body: 'post two'}
 ];
 
-function createPost(post) {
-    setTimeout(function () {
-        posts.push(post);
-    }, 2000);
-}
-
-function getPosts() {
-    setTimeout(function () {
-        let output = '';
-
-        posts.forEach(post => {
-            output += `<li>${post.title}</li>`
-        });
-        document.body.innerHTML = output;
-    }, 1000)
-}
-
-createPost({ title: 'Post Three', body: 'This is post three.' });
-
-getPosts();
