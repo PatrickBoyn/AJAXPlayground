766d026c518b14e531c024e2ea900563372b1746
Patrick Boynton <dakilangmanok@gmail.com>
2019-06-29 21:50:23 -0400

    opened the xhr connection to the API.

--- Lesson3/app.js
+++ Lesson3/app.js
@@ -2,10 +2,11 @@ document.querySelector('.get-jokes').addEventListener('click', getJokes);
 
 
 function getJokes(e) {
-    console.log('get jokes!');
     const number = document.querySelector('input[type="number"]').value;
 
-    console.log(number);
+    const xhr = new XMLHttpRequest();
+
+    xhr.open(`http://api.icndb.com/jokes/random/${number}`, true);
 
     e.preventDefault();
 }
