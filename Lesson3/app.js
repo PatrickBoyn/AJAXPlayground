12f32204e41b7e3f327edd3ab909cab36bcd8800
Patrick Boynton <dakilangmanok@gmail.com>
2019-06-29 21:45:13 -0400

    Added the query selector to get the input.

--- Lesson3/app.js
+++ Lesson3/app.js
@@ -3,6 +3,7 @@ document.querySelector('.get-jokes').addEventListener('click', getJokes);
 
 function getJokes(e) {
     console.log('get jokes!');
-
+    const number = document.querySelector('input[type="number"]');
+    
     e.preventDefault();
 }
