8181ca7d2cb5127900e4ec70f8a53e1adeae1a5d
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-01 15:14:12 -0400

    Removed the setTimeout function.

--- Lesson4/app.js
+++ Lesson4/app.js
@@ -1,23 +1,18 @@
 const posts = [
-    { title: 'Post one', body: 'post one' },
-    {title: 'Post two', body: 'post two'}
+  { title: 'Post one', body: 'post one' },
+  { title: 'Post two', body: 'post two' }
 ];
 
-function createPost(post, callBack) {
-    setTimeout(function () {
-        posts.push(post);
-        callBack();
-    }, 2000);
-}
+function createPost(post, callBack) {}
 
 function getPosts() {
-    setTimeout(function () {
-        let output = '';
-        posts.forEach(post => {
-            output += `<li>${post.title}</li>`
-        });
-        document.body.innerHTML = output;   
-    }, 1000);
+  setTimeout(function() {
+    let output = '';
+    posts.forEach(post => {
+      output += `<li>${post.title}</li>`;
+    });
+    document.body.innerHTML = output;
+  }, 1000);
 }
 
-createPost({title: 'Post three', body: 'post three'}, getPosts);
+createPost({ title: 'Post three', body: 'post three' }, getPosts);
